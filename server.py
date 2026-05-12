19ec6335cb1e0489575f37876f28ad329d4c97ef
shashlik <shashlichok17@gmail.com>
2026-05-12 09:29:11 +0300

    fixed-bugs

--- server.py
+++ server.py
@@ -1,13 +1,25 @@
 import socket
-import threading
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = input('Enter server address: ')
-port = int(input('Enter server port: '))
+server_address = input("Enter server address: ")
+port = int(input("Enter server port: "))
 
-server.bind((server_address, int(port)))
+server.bind((server_address, port))
 
 server.listen()
 
-print(f"Listening on {server_address}")
+print(f"Listening on {server_address}:{port}")
+
+while True:
+    client, addr = server.accept()
+
+    print(f"Connected: {addr}")
+
+    data = client.recv(1024).decode()
+
+    print(data)
+
+    client.send("Hello client".encode())
+
+    client.close()
