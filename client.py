0531bd0aa9eb1b2a76c514cda645947d37398f16
shashlik <shashlichok17@gmail.com>
2026-05-12 10:04:23 +0300

    full-rewrited-code-and-fixed-bugs

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 import socket
 import json
+import threading
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-with open("data.json", "r") as f:
+with open("data.json", "r", encoding="utf-8") as f:
     data = json.load(f)
+
 
 def register():
     nickname = input("Enter your nickname: ")
@@ -13,49 +15,75 @@ def register():
         print("Nickname must be between 3 and 20 characters!")
         nickname = input("Enter your nickname: ")
 
-    data["nickname"] = nickname
-
     password = input("Enter your password: ")
 
     while len(password) < 4 or len(password) > 20:
         print("Password must be between 4 and 20 characters!")
         password = input("Enter your password: ")
 
+    data["nickname"] = nickname
     data["password"] = password
 
     with open("data.json", "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
-if data["nickname"] != "":
+if data.get("nickname"):
     password = input("Enter your password: ")
 
     while data["password"] != password:
         print("Password is incorrect!")
         password = input("Enter your password: ")
-
-    print("Login completed.")
-
 else:
     register()
 
 connected = False
-ip = ""
-port = ""
 
 while not connected:
     ip = input("Enter server IP: ")
-    port = input("Enter server port: ")
+    port = int(input("Enter server port: "))
 
     try:
-        client.connect((ip, int(port)))
+        client.connect((ip, port))
         connected = True
         print("Connected successfully.")
+    except:
+        print("Cannot connect to server!")
 
-    except ValueError:
-        print("Port must be a number!")
-        continue
 
-    except (socket.gaierror, ConnectionRefusedError):
-        print("Cannot connect to the server!")
-        continue
+def send_json(obj):
+    client.send(json.dumps(obj).encode())
+
+
+def receive():
+    while True:
+        try:
+            msg = client.recv(1024).decode()
+            if not msg:
+                break
+
+            data = json.loads(msg)
+
+            if data["type"] == "message":
+                print(f"{data['from']}: {data['text']}")
+
+            if data["type"] == "system":
+                print(f"[SYSTEM] {data['text']}")
+        except:
+            break
+
+
+if client.recv(1024).decode() == "NICK":
+    send_json({
+        "type": "nick",
+        "nickname": data["nickname"]
+    })
+
+threading.Thread(target=receive, daemon=True).start()
+
+while True:
+    text = input()
+    send_json({
+        "type": "message",
+        "text": text
+    })
--- server.py
+++ server.py
@@ -1,25 +1,135 @@
 import socket
+import threading
+import json
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = input("Enter server address: ")
-port = int(input("Enter server port: "))
+with open("banned.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
 
-server.bind((server_address, port))
+banned = set(data.get("banned", []))
+muted = set()
+clients = {}
 
+server.bind(("0.0.0.0", 5555))
 server.listen()
 
-print(f"Listening on {server_address}:{port}")
 
-while True:
-    client, addr = server.accept()
+def save_bans():
+    with open("banned.json", "w", encoding="utf-8") as f:
+        json.dump({"banned": list(banned)}, f, ensure_ascii=False, indent=4)
 
-    print(f"Connected: {addr}")
 
-    data = client.recv(1024).decode()
+def send(conn, obj):
+    try:
+        conn.send(json.dumps(obj).encode())
+    except:
+        pass
 
-    print(data)
 
-    client.send("Hello client".encode())
+def broadcast(obj, skip=None):
+    for c in list(clients.values()):
+        if c != skip:
+            send(c, obj)
 
-    client.close()
+
+def handle_client(conn):
+    nickname = ""
+
+    try:
+        conn.send("NICK".encode())
+
+        msg = conn.recv(1024).decode()
+        data_msg = json.loads(msg)
+
+        if data_msg["type"] == "nick":
+            nickname = data_msg["nickname"]
+
+        if nickname in banned:
+            send(conn, {"type": "system", "text": "banned"})
+            conn.close()
+            return
+
+        clients[nickname] = conn
+
+        broadcast({"type": "system", "text": f"{nickname} joined"})
+
+        while True:
+            try:
+                msg = conn.recv(1024).decode()
+                if not msg:
+                    break
+
+                data_msg = json.loads(msg)
+
+                if data_msg["type"] == "message":
+                    if nickname in muted:
+                        continue
+
+                    broadcast({
+                        "type": "message",
+                        "from": nickname,
+                        "text": data_msg["text"]
+                    }, skip=conn)
+
+            except:
+                break
+
+    finally:
+        if nickname in clients:
+            del clients[nickname]
+
+        conn.close()
+
+        if nickname:
+            broadcast({"type": "system", "text": f"{nickname} left"})
+
+
+def accept_clients():
+    while True:
+        conn, addr = server.accept()
+        threading.Thread(target=handle_client, args=(conn,), daemon=True).start()
+
+
+def handle_command(cmd):
+    parts = cmd.split()
+
+    if len(parts) < 2:
+        return
+
+    action = parts[0]
+    target = parts[1]
+
+    if action == "list":
+        print(list(clients.keys()))
+        return
+
+    if target not in clients:
+        return
+
+    conn = clients[target]
+
+    if action == "kick":
+        send(conn, {"type": "system", "text": "kicked"})
+        conn.close()
+        del clients[target]
+
+    elif action == "ban":
+        banned.add(target)
+        save_bans()
+        send(conn, {"type": "system", "text": "banned"})
+        conn.close()
+        del clients[target]
+
+    elif action == "mute":
+        muted.add(target)
+
+
+def command_loop():
+    while True:
+        cmd = input("> ")
+        handle_command(cmd)
+
+
+threading.Thread(target=accept_clients, daemon=True).start()
+command_loop()
